7b5becf3596fab8f855653443cda82747348c75b
Greg Stupp <stuppie@ufl.edu>
2014-08-14 11:27:00 -0400

    Update maps.html with date last updated. Push changes to server over ftp

--- getTop.py
+++ getTop.py
@@ -29,8 +29,8 @@ def getNewTop100():
     
     # read in html table as df
     top = pd.read_html(str(table),infer_types=False)[0]
-    top['URL']=links
-    top['#']=top['#'].astype(int)
+    top['URL'] = links
+    top['#'] = top['#'].astype(int)
     del top['Difficulty']
     del top['Rating']
     top = top.to_dict("records") # to a list of dicts
@@ -64,14 +64,14 @@ def getTrailInfo(trail):
     trail['html'] = str(f.read())
 
     soup = BeautifulSoup(trail['html'])
-    stat = soup.findAll('div',attrs={"class":"\\\'st_stat1\\\'"})
+    stat = soup.findAll('div',attrs = {"class":"\\\'st_stat1\\\'"})
     trail['distance'] = int(stat[0].getText().strip(string.ascii_letters))
     try:
-        trail['ascent'], trail['descent']=[int(x.replace(',','')) for x in re.findall("[-,0-9]+",stat[3].getText())]
+        trail['ascent'], trail['descent'] = [int(x.replace(',','')) for x in re.findall("[-,0-9]+",stat[3].getText())]
     except:
         trail['ascent'], trail['descent'] = [0, 0]
-    trail['rating'] = float(soup.find('span',attrs={"class":"\\\'average\\\'"}).getText())
-    stat = soup.findAll('div',attrs={"class":"span8"})[2].findAll("div")[0]
+    trail['rating'] = float(soup.find('span',attrs = {"class":"\\\'average\\\'"}).getText())
+    stat = soup.findAll('div',attrs = {"class":"span8"})[2].findAll("div")[0]
     trail['picURL'] = re.findall("(http://\S*.jpg)", str(stat))[0]
 
     latlng = re.findall("google.maps.LatLng\(\S\S(-?\d+[.]\d+)\S\S[,]\s\S{2}(-?\d+[.]\d+)",trail['html'])
@@ -81,6 +81,9 @@ def getTrailInfo(trail):
         latlng = [0.,0.]
     
     (trail['Lat'], trail['Lng']) = latlng
+    
+    des = soup.find('div', attrs = {'id':"\\\'st_description\\\'"}).getText()  
+    trail['description'] = des.replace('\\n','').replace('\\t','').replace("\\'","'")
     return trail
     
     #trailHTML[83832:83832+50]
@@ -103,6 +106,43 @@ def showTopX(coll, x):
 def getNumEntries(coll):
     return coll.find().count()
 
+def addDescription(coll):
+    #added new field. use this to update db without redoing redownloading everything.
+    for trail in coll.find():
+        soup = BeautifulSoup(trail['html'])
+        des = soup.find('div', attrs={'id':"\\\'st_description\\\'"}).getText()  
+        trail['description'] = des.replace('\\n','').replace('\\t','').replace("\\'","'")
+        coll.update({'URL':trail['URL']}, {"$set": {"description":trail['description']}})
+
+def updateLastUpdated(pathToMaps):
+    #input: path to maps.html
+    import datetime
+
+    with open(pathToMaps, 'r') as f:
+        mapsHTML = f.read()
+    
+    soup = BeautifulSoup(mapsHTML)
+    soup.find('span',attrs={'id':'last_updated'}).string  = str(datetime.date.today())
+    
+    html = soup.prettify("utf-8")
+    with open(pathToMaps, "wb") as file:
+        file.write(html)
+
+def uploadChanges(dirPath):
+    import ftplib
+    #Get password from config file so we don't share it with the world
+    with open('password.txt', 'r') as f:
+        password = f.read()
+    session = ftplib.FTP('stuppie.com','stuppie', password)
+    file = open(dirPath + os.path.sep + 'map.html','rb')                  # file to send
+    session.storbinary('STOR map.html', file)     # send the file
+    file.close()                                    # close file and FTP
+    file = open(dirPath + os.path.sep + 'markers.js','rb')                  # file to send
+    session.storbinary('STOR markers.js', file)     # send the file
+    file.close()  
+    
+    print(session.quit())
+    
 if False:
     # insert a bunch of records at once    
     records = json.loads(top.to_json(orient="records"))
@@ -111,7 +151,15 @@ if False:
     for d in test:
         coll.update({'URL':d['URL']}, d, upsert = True)
 
-if __name__ == "X__main__":
+if __name__ == "__main__":
     coll = getMongo()
     top = getNewTop100()
     updateDB(coll, top)
+    
+    dirPath = r'C:\Users\Greg\Documents\stuppie.com'
+    with open(dirPath + os.path.sep + "markers.js", "w") as text_file:
+        print("var markers = " + dumps(list(coll.find({'#':{'$lte':100}},{'html':0, "_id":0}).sort('#',1))), file = text_file)
+    
+    updateLastUpdated(dirPath + os.path.sep + 'map.html')
+    uploadChanges(dirPath)
+    
